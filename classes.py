ac3d54f826a19c4eeedd8fe5ae5cd754c9c024dd
Rashid Efendiev <rashidefendiev0505@gmail.com>
2025-12-24 10:44:31 +0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -4,6 +4,7 @@ import ask_gpt
 import find_nearst_points
 import get_database
 import json
+from loguru import logger
 
 class Object:
     def __init__(self, x, y, id, street, desc, other_params):
@@ -47,129 +48,122 @@ class EmbSearch:
         return res
 
 class LLMAgent:
-    def __init__(self, tools, db, embs):
+    def __init__(self):
         self.model = ask_gpt.QwenChat()
-        self.tools = tools
-        self.db = db
-        self.embs = embs
+        self.tools = [{
+            "type": "function",
+            "function" : {
+                "name": "get_places",
+                "parameters": {
+                    "type": "object",
+                    "description": "Функция получает строку запроса и возвращает 10 самых подходящих точек под запрос",
+                    "properties": {
+                        "query": {"type": "string"}
+                    },
+                    "required": ["query"]
+                }
+            }},
+            {"type" : "function",
+            "function" : {
+                "name": "message",
+                "parameters": {
+                    "type": "object",
+                    "description": "В эту функцию нужно передать точки для маршрута в формате: адрес, id точки",
+                    "properties": {
+                        "text": {
+                            "type": "string",
+                            "description": "Описание маршрута"
+                        },
+                        "points": {
+                            "type": "array",
+                            "description": "список id точек маршрута",
+                            "items": {
+                                "type": "integer"
+                            },
+                        }
+                    },
+                    "required": ["text", "points"]
+                }
+            }
+        }]
+        self.db = get_database.get_database()
+        self.embs = EmbSearch(self.db, 50)
 
     def get_places(self, query : str):
+        # logger.info(query)
         res_points = find_nearst_points.get_points(self.db, self.embs, self.a, self.b, query)
         ans = ''
         for i in res_points:
             ans += 'id: ' + str(i.id) + ', adress: ' + i.street + ', description: ' + i.desc + '\n'
+        # # logger.info(ans)
+        # logger.info(res_points)
         return ans
-    
-    def message(text, points):
-        return [text, points]
+
+    def message(self, text, points):
+        # logger.info(text, points)
+        return text, points
 
     def answer_model(self):
-        name, args = self.model.ask(self.system_prompt, self.tools)
+        name, args, id = self.model.ask(self.system_prompt, self.tools)
 
         if name == "get_places":
             # self.system_prompt = self.get_place(**args)
             result = self.get_places(**args)
         elif name == "message":
-            result = self.message(**args)
-            self.ans.append(result)
-        return result
+            # logger.info(args)
+            # print(args)
+            desc = args['text']
+            points = args['points']
+            # logger.info(desc)
+            # logger.info(points)
+
+            result = self.message(desc, points)
+            self.desc, self.ans_id = result
+        return result, id
 
     def get_answer(self, a, b, w_type, time):
         self.system_prompt = f"Ты находишься в России, на Федеральной территории Сириус. \
-            Требуется построить пешеходный маршрут для {w_type}, который начинается в адресе \
-            {a.street} и заканчивается в адресе {b.street}. Длительность маршрута должна \
-            быть равна примерно {time} минут. Маршрут должен содержать не более 5 промежуточных \
-            точек и быть интересным и наиболее подходящим для данного случая. Желательно не \
-            заходить повторно в заведения одинакового типа (наприимер, в два кафе). Все точки \
-            должны быть уникальными (в том числе ни одна промежуточная точка не должна совпадать \
-            с точками {a.street} и {b.street}! Ты можешь спрашивать какие есть подходящие точки, \
-            сделав запрос get_places. В качестве аргумента передай, какого типа точки тебе нужны. \
-            Функция вернет тебе адреса, краткие описания и id всех подходящих заведений. \
-            Когда будешь готов добавить точку в маршрут, вызови функцию message и передай ей адрес и id точки"
+Требуется построить пешеходный маршрут для цели: \"{w_type}\", который начинается в адресе \
+{a.street} и заканчивается в адресе {b.street}. Длительность маршрута должна \
+быть равна примерно {time} минут. Маршрут должен содержать не более 5 промежуточных \
+точек и быть интересным и наиболее подходящим для данного случая. В маршруте не должно быть \
+больше одного кафе или ресторана, если об этом не просит пользователь! \
+Все точки должны быть уникальными (в частности, ни одна промежуточная точка не должна совпадать \
+с точками {a.street} и {b.street})! Ты можешь спрашивать какие есть подходящие точки, \
+сделав запрос get_places. В качестве аргумента передай, какого типа точки тебе нужны. \
+Функция вернет тебе адреса, краткие описания и id всех подходящих заведений. \
+Когда будешь готов добавить точку в маршрут, вызови функцию message и передай ей адреса и массив id точек. \
+За раз можно сделать только один запрос к функциям. Всего, прежде чем выдать ответ, сделай не более 5 запросов. \
+После 5 запросов обязательно выведи ответ, если не сделал этого раньше!!! \
+Начальную и конечную точку не нужно добавлять в маршрут!"
+# Маршрут обязательно должен начинаться в точке {a.id} и заканчиваться в точке {b.id}!!!"
+
+        # logger.info(self.system_prompt)
 
         self.a = a
         self.b = b
         self.w_type = w_type
         self.time = time
+        self.ans_id = []
         self.ans = []
+        self.desc = 'no-description'
         cnt = 0
-        while cnt < 10 and len(self.ans) < 5:
+        while cnt < 10 and len(self.ans_id) == 0:
             cnt += 1
 
-            print(self.system_prompt)
-            res_gpt = self.answer_model()
-            print(res_gpt)
+            # print(self.system_prompt)
+            res_gpt, id = self.answer_model()
+            # print(res_gpt)
 
             self.model.messages.append({
                 "role": "tool",
-                "tool_call_id": self.model.tool_call.id,
+                "tool_call_id": id, # self.model.tool_call.id,
                 "content": json.dumps(res_gpt, ensure_ascii=False)
             })
+            self.system_prompt = ""
         self.model.clear_history()
-        return self.ans
 
-def main():
-    db = get_database.get_database()
+        for i in self.ans_id:
+            self.ans.append(self.db[i])
 
-    a = db[15]
-    b = db[150]
-    w_type = "прогулка с друзьями"
-    time = "2"
-
-    embs = EmbSearch(db, 150)
-
-    tools = [{
-        "type": "function",
-        "function" : {
-            "name": "get_places",
-            "parameters": {
-                "type": "object",
-                "description": "Функция получает строку запроса и возвращает 10 самых подходящих точек под запрос",
-                "properties": {
-                    "query": {"type": "string"}
-                },
-                "required": ["query"]
-            }
-        }},
-        {"type" : "function",
-        "function" : {
-            "name": "message",
-            "parameters": {
-                "type": "object",
-                "description": "В эту функцию нужно передать точки для маршрута в формате: адрес, id точки",
-                "properties": {
-                    "text": {"type": "string"},
-                    "points": {
-                        "type": "array",
-                        "items": {
-                            "type": "integer"
-                        },
-                        # "description": "id каждой точки в маршруте",
-                    }
-                },
-                "required": ["text", "points"]
-            }
-        }
-    }]
-
-
-    # with open("points_embeddings", 'w', encoding='utf-8') as f:
-    #     for i in embs.emb:
-    #         for j in i:
-    #             f.write(str(j))
-    #             f.write(' ')
-    #         f.write('\n')
-
-    model = LLMAgent(tools, db, embs)
-    ans = model.get_answer(a, b, w_type, time)
-    print(ans)
-
-    for i in ans:
-        print(db[i].street)
-
-    # print(len(ans))
-    # for i in ans:
-    #     print(i)
-
-if __name__ == '__main__':
-    main()
+        return self.desc, self.ans
