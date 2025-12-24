5c424a7a6d908afdcd5d978c267ff9e1bed0041a
Rashid Efendiev <rashidefendiev0505@gmail.com>
2025-12-24 10:51:54 +0300

    Update get_embedding.py

--- get_embedding.py
+++ get_embedding.py
@@ -5,13 +5,13 @@ from sentence_transformers import SentenceTransformer
 model = SentenceTransformer("BAAI/bge-m3")
 
 def get_emb(sentences):
-    return model.encode(sentences)
+    return model.encode(sentences, batch_size=1)
 
 def get_simularity(embeddings, q_embeddings):
     return model.similarity(embeddings, q_embeddings)
 
-def NN(embeddings, k, dist):
-    neigh = NearestNeighbors(n_neighbors=k, radius=dist)
+def NN(embeddings, k):
+    neigh = NearestNeighbors(n_neighbors=k)
     neigh.fit(embeddings)
     return neigh
 
