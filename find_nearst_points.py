235bc131d153b8893012430d28df20231d441c65
Rashid Efendiev <rashidefendiev0505@gmail.com>
2025-12-25 10:02:01 +0300

    Update find_nearst_points.py

--- find_nearst_points.py
+++ find_nearst_points.py
@@ -4,7 +4,7 @@ import get_points_by_dist
 
 def get_points(db, qemb_s, p_start, p_end, pipe):
     resd = get_points_by_dist.get_points_into_route(db, p_start, p_end, 50)
-
+    # logger.info(resd)
     resd_embs = classes.EmbSearch(resd, 6, qemb_s)
     resd_final = resd_embs.search(pipe)
 
@@ -13,5 +13,13 @@ def get_points(db, qemb_s, p_start, p_end, pipe):
     for i in resq:
         resd_final.append(i)
 
+    # logger.info(resd_final)
     ans = list(dict.fromkeys(resd_final))
-    return ans
+
+    dists_to_start = []
+    dists_to_end = []
+    for i in ans:
+        dists_to_start.append(i.dist_between_points(p_start))
+        dists_to_end.append(i.dist_between_points(p_end))
+
+    return ans, dists_to_start, dists_to_end
